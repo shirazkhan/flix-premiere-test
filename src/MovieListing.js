a383900044ba3ed59a919c9a82b55f9eb34fc52d
Shiraz Khan <sshirazz@gmail.com>
2020-09-15 15:20:39 +0100

    Create handleApi and renderMovies functions.

--- src/MovieListing.js
+++ src/MovieListing.js
@@ -7,10 +7,25 @@ export default function MovieListing() {
 
     const [apiData,setApiData] = useState(null);
 
+    const handleApi = () => { // Function to fetch and store api data to apiData state.
+        axios.get(API_URL)
+        .then(res => setApiData(res.data))
+        .catch(err => console.log(err))
+    }
+
+    const renderMovies = () => { // Function to render <li> items with movie info.
+
+        if(apiData !== null){ // If apiData has the information
+            apiData.films.map(film => {
+                return <li key = {film.id}>{`${film.title} (${film.duration_seconds} Seconds)`}</li>
+            })
+        }
+    }
+
     return (
         <>
             <ul>
-
+                {apiData === null ? "" : renderMovies()}
             </ul>
         </>
     )
