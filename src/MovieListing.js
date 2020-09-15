35146635b61aa01293b97465c7352136f9f370a6
Shiraz Khan <sshirazz@gmail.com>
2020-09-15 15:11:44 +0100

    Create apiData state

--- src/MovieListing.js
+++ src/MovieListing.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+const API_URL = "https://api.flixpremiere.com/v1/films/filter/now_showing?limit=10";
+
 export default function MovieListing() {
+
+    const [apiData,setApiData] = useState(null);
+
     return (
         <>
-            I AM MovieListing
+            <ul>
+
+            </ul>
         </>
     )
 }
